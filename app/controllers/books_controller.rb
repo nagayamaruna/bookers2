3556b56864820ef9fbe91d8783ce2782b06f295e
EC2 Default User <ec2-user@ip-172-31-0-205.ap-northeast-1.compute.internal>
2024-06-12 13:34:49 +0000

    showページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,15 @@ class BooksController < ApplicationController
     @user = current_user
   end
   
+  def edit
+  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
   private
   
   def book_params
