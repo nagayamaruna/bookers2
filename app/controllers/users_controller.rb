9ea4db7c45ecdf2d5301c41158d5d610287247c1
EC2 Default User <ec2-user@ip-172-31-0-205.ap-northeast-1.compute.internal>
2024-06-13 14:37:20 +0000

    テンプレート化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
     #@books = @user.books
   end
 
-  def edit
+  def index
+    @book = Book.new
+    @user = current_user
   end
   
+  def edit
+  end
+
   private
   
   def user_params
