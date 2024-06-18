0275624c31afde3bfc49b76a43450cbc9ffa897c
EC2 Default User <ec2-user@ip-172-31-0-205.ap-northeast-1.compute.internal>
2024-06-18 12:13:29 +0000

    バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,9 +36,14 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
     flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book)
+    else
+      @book = book
+      @user = current_user
+      render :edit
+    end
   end
   
   def destroy
